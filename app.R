a3771556c5d3b0a2476c1e0e270bf25f0e821fda
Tyler Hill <tylerhill90@gmail.com>
2021-02-20 15:19:15 -0800

    isolated button to generate random data only when clicked

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ ui <- dashboardPage(
   dashboardBody(
   sliderInput(inputId = "num",
               label = "How many points?",
-              min = 1, max = 1000, value = 500),
+              min = 100, max = 1000, value = 500),
   sliderInput(inputId = "centroids",
               label = "How many centroids?",
               min = 1, max = 5, value = 2),
@@ -20,47 +20,49 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
-  output$scat <- renderPlot({
-    input$run
-    
-    cents <- input$centroids
-    points <- input$num
-    
-    bins <- points %/% cents
-    mod <- points %% cents
-    last_bin <- bins + mod
-    
-    x_s <- vector("list", points)
-    y_s <- vector("list", points)
-
-    mu_x <- sample(-10:10, cents)
-    mu_y <- sample(-10:10, cents)
-    sd <- sample(1:2, cents, replace = TRUE)
-    
-    i <- 0
-    for (bin in 1:cents) {
-      if (bin != cents) {
-        x_data <- rnorm(bins, mu_x[bin], sd[bin])
-        y_data <- rnorm(bins, mu_y[bin], sd[bin])
-        all_data <- bins
-      } else {
-        x_data <- rnorm(last_bin, mu_x[bin], sd[bin])
-        y_data <- rnorm(last_bin, mu_y[bin], sd[bin])
-        all_data <- bins
+  observeEvent(input$run, {
+      cents <- input$centroids
+      points <- input$num
+      
+      bins <- points %/% cents
+      mod <- points %% cents
+      last_bin <- bins + mod
+      
+      x_s <- vector("list", points)
+      y_s <- vector("list", points)
+      
+      mu_x <- sample(-10:10, cents)
+      mu_y <- sample(-10:10, cents)
+      sd_x <- sample(seq(1, 2, 0.2), cents, replace = TRUE)
+      sd_y <- sample(seq(1, 2, 0.2), cents, replace = TRUE)
+      
+      i <- 0
+      for (bin in 1:cents) {
+        if (bin != cents) {
+          x_data <- rnorm(bins, mu_x[bin], sd_x[bin])
+          y_data <- rnorm(bins, mu_y[bin], sd_y[bin])
+          all_data <- bins
+        } else {
+          x_data <- rnorm(last_bin, mu_x[bin], sd_x[bin])
+          y_data <- rnorm(last_bin, mu_y[bin], sd_y[bin])
+          all_data <- bins
+        }
+        for (j in 1:all_data) {
+          i <-  i + 1
+          x_s[[i]] <- x_data[j]
+          y_s[[i]] <- y_data[j]
+        }
       }
-      for (j in 1:all_data) {
-        i <-  i + 1
-        x_s[[i]] <- x_data[j]
-        y_s[[i]] <- y_data[j]
-      }
-    }
-    x_s <- unlist(x_s)
-    y_s <- unlist(y_s)
-    
-    ggplot() +
-      aes(x_s, y_s) +
-      geom_point()
+      x_s <- unlist(x_s)
+      y_s <- unlist(y_s)
+      
+      output$scat <- renderPlot({
+        ggplot() +
+          aes(x_s, y_s) +
+          geom_point()
+      })
   })
+
 }
 
 shinyApp(ui = ui, server = server)
